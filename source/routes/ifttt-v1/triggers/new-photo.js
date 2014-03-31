966c18b892c01712998afa2a4d428682d961ceb4
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-03-31 17:05:16 +0200

    Clean up of "new-photo" trigger.

--- source/routes/ifttt-v1/triggers/new-photo.js
+++ source/routes/ifttt-v1/triggers/new-photo.js
@@ -3,8 +3,6 @@ var errorMessages = require('../../../utils/error-messages.js');
 var versionPath = '/ifttt/v1/';
 var triggerPath = versionPath + 'triggers/new-photo';
 
-//var debug = require('../../../utils/debug.js');
-
 module.exports = function setup(app) {
 
   app.post(triggerPath + '/fields/stream/options',
@@ -13,18 +11,11 @@ module.exports = function setup(app) {
   app.post(triggerPath, function (req, res /*, next*/) {
     if (! req.pryvConnection) { return errorMessages.sendAuthentificationRequired(res); }
 
-
-    var limit = req.body.limit || 50;
-
-    var modifiedSince =  Number.MIN_VALUE; // TODO set with last fetched events from db
-
     var filterLike = {
       streams : [ req.body.triggerFields.stream],
-      modifiedSince: modifiedSince,
-      fromTime: Number.MIN_VALUE
+      limit: req.body.limit || 50,
+      types: ['picture/attached']
     };
-
-    var lastFoundEvent = modifiedSince;
 
     req.pryvConnection.events.get(filterLike, function (error, eventsArray) {
       if (error) { return errorMessages.sendInternalError(res, 'Failed fetching events'); }
@@ -32,29 +23,23 @@ module.exports = function setup(app) {
       var data = [];
 
       for (var i = 0, length = eventsArray.length; i < length; i++) {
-        if (data.length >= limit) { break; }
         var event = eventsArray[i];
 
         if (event.type === 'picture/attached' && event.attachments && event.attachments.length > 0)
         {
-          lastFoundEvent = event.modifiedSince;
           data.push({
-            ifttt: {id : event.id, timestamp: Math.round(event.time)}, //TODO can we keep microsecs
+            ifttt: {id : event.id, timestamp: Math.round(event.time)},
             ImageURL : event.attachmentUrl(event.attachments[0]),
             Tags: event.tags ? event.tags.join(', ') : null,
-            StreamName: event.streamId, // TODO switch to name one we have stream chaching
+            StreamName: event.streamId, // TODO switch to name one we have stream caching
             At: (new Date(event.time * 1000)).toISOString()
           });
         }
       }
 
       console.log(data);
-
-      // TODO save lastFoundEvent into modifiedSince for this request for future usage
       res.send({data : data});
-
     });
-
   });
 };
 
