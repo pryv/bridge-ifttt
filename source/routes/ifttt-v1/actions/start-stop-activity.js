4948794b60a2db4cb07199744c88ac292f6ca5e7
Perki <pm@perki.com>
2014-05-01 10:27:13 +0200

    drafting start / stop

---
+++ source/routes/ifttt-v1/actions/start-stop-activity.js
@@ -0,0 +1,58 @@
+//var pryv = require('pryv');
+
+var PYError = require('../../../errors/PYError.js');
+var versionPath = '/ifttt/v1/';
+
+// Draft
+// - the lib-js to support / start / stop
+// - stream list should display singleActivities indicators
+
+
+exports.setup = function setup(app) {
+  var actionPath = versionPath + 'actions/start-stop-activity';
+
+  app.post(actionPath + '/fields/streamId/options',
+    require('../../../fields/stream').optionsStrict);
+
+  app.post(actionPath, function (req, res, next) {
+    if (! req.pryvConnection) { return next(PYError.authentificationRequired()); }
+
+    if (! req.body.actionFields) {
+      return next(PYError.contentError('Cannot find actionFields'));
+    }
+
+    var actionFields = req.body.actionFields;
+
+    if (! actionFields.streamId) {
+      return next(PYError.contentError('Cannot find actionFields.streamId'));
+    }
+
+
+    if (! actionFields.action || ['start', 'stop'].indexOf(actionFields.action) < 0) {
+      return next(PYError.contentError('Cannot find (or invalid) actionFields.action'));
+    }
+
+
+    // --- streamId
+    var streamId = actionFields.streamId; //TODO check it's valid
+    var eventData = {streamId: streamId};
+
+    // --- description
+    if (actionFields.description) { eventData.description = actionFields.description; }
+
+    // --- tags
+    if (actionFields.tags) {
+      var tags = [];
+      actionFields.tags.split(',').forEach(function (tag) {
+        tags.push(tag.trim());
+      });
+      if (tags.length > 0) {
+        eventData.tags = tags;
+      }
+    }
+
+    // -- to be continued
+
+  });
+};
+
---
+++ test/acceptance/actions/start-stop-activity.js
@@ -0,0 +1,61 @@
+/*global describe, before, it */
+var config = require('../../../source/utils/config'),
+  db = require('../../../source/storage/database'),
+  request = require('superagent');
+
+var testData = require('../../test-data.js');
+
+require('../../../source/server');
+
+require('readyness/wait/mocha');
+
+require('should');
+
+var serverBasePath = 'http://' + config.get('http:ip') + ':' + config.get('http:port');
+
+describe('/actions/start-stop-activity', function () {
+  this.timeout(5000);
+  before(function () {
+    db.setSet(testData.oauthToken, testData.userAccess);
+  });
+
+  describe('/', function () {
+
+    it('Start new activity in stream', function (done) {
+      request.post(serverBasePath + '/ifttt/v1/actions/start-stop-activity')
+        .set('Authorization', 'Bearer ' + testData.oauthToken).send(
+        { actionFields: {
+          description: 'Na Na Nah',
+          streamId: testData.streamId,
+          action: 'start',
+          tags: 'IFTTT'
+        }
+        }).end(function (res) {
+          res.should.have.status(200);
+          res.body.should.have.property('data');
+          res.body.data.should.be.an.instanceof(Array);
+          res.body.data[0].should.have.property('id');
+          done();
+        });
+    });
+
+    it('Stop any activity in stream', function (done) {
+      request.post(serverBasePath + '/ifttt/v1/actions/start-stop-activity')
+        .set('Authorization', 'Bearer ' + testData.oauthToken).send(
+        { actionFields: {
+          description: 'Na Na Nah',
+          streamId: testData.streamId,
+          action: 'stop',
+          tags: 'IFTTT'
+        }
+        }).end(function (res) {
+          res.should.have.status(200);
+          res.body.should.have.property('data');
+          res.body.data.should.be.an.instanceof(Array);
+          res.body.data[0].should.have.property('id');
+          done();
+        });
+    });
+
+  });
+});
