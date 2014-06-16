a102dac3bc9147e661ff647dd5c6d5123bbf96db
Perki <pm@perki.com>
2014-06-16 10:55:55 +0200

    removed md => html for notes

--- source/routes/ifttt-v1/triggers/new-note.js
+++ source/routes/ifttt-v1/triggers/new-note.js
@@ -1,11 +1,7 @@
-var marked = require('marked');
-marked.setOptions({
-  sanitize: true
-});
 
 module.exports = function setup(app) {
   require('./new-event-generic').setup(app, 'new-note', 'note/txt', function (event, map) {
-    map.NoteContent = marked(event.content); // Does md to HTML
+    map.NoteContent = event.content;
     return true;
   });
 };
