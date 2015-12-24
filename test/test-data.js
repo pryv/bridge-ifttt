a4b3b3a01b0029746dedf0671b04372791cf57de
Perki <pm@perki.com>
2015-12-24 09:44:14 +0100

    tests on prod

--- test/test-data.js
+++ test/test-data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  userAccess : {username: 'ifttttest', pryvToken: 'cht8va9t9001he943bk8o4dhu'},
+  userAccess : {username: 'ifttttest', pryvToken: 'ciijzuln2057akdq3bv3vhjya'},
   oauthToken : 'OI2O98AHF9A',
   streamId : 'chtisiuo4000c0i43ys0czem0'
 };
