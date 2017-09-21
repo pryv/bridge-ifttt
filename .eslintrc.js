1bd4886e8b2f4b2e05a0140e11887ccfd23aa881
Kaspar Schiess <kaspar@pryv.com>
2017-09-21 17:44:41 +0200

    Add proper eslinting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
+    "parser": "babel-eslint",
+    "plugins": [
+      "flowtype"
+    ],
     "env": {
         "es6": true,
         "node": true
     },
     "extends": [
         "eslint:recommended",
+        "plugin:flowtype/recommended"
     ],
     "parserOptions": {
         "sourceType": "module"
@@ -12,7 +17,7 @@ module.exports = {
     "rules": {
         "indent": [
             "warn",
-            2, 
+            2,
             { "VariableDeclarator": { "var": 2, "let": 2, "const": 3 } },
         ],
         "linebreak-style": [
